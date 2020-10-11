0b1bbaee0b061c718fc4168f2c8fcdc3a377bd0f
Andreas Eger <dev@eger-andreas.de>
2020-10-11 14:55:04 +0200

    update values

--- secrets/generate-secrets.sh
+++ secrets/generate-secrets.sh
@@ -20,43 +20,17 @@ set +o allexport
 
 PUB_CERT="${REPO_ROOT}/secrets/pub-cert.pem"
 
-# Helper function to generate secrets
-kseal() {
-  echo "------------------------------------"
-  # Get the path and basename of the txt file
-  # e.g. "deployments/default/pihole/pihole-helm-values"
-  secret="$(dirname "$@")/$(basename -s .txt "$@")"
-  echo "Secret: ${secret}"
-  # Get the filename without extension
-  # e.g. "pihole-helm-values"
-  secret_name=$(basename "${secret}")
-  echo "Secret Name: ${secret_name}"
-  # Extract the Kubernetes namespace from the secret path
-  # e.g. default
-  namespace="$(echo "${secret}" | awk -F /deployments/ '{ print $2; }' | awk -F / '{ print $1; }')"
-  echo "Namespace: ${namespace}"
-  # Create secret and put it in the applications deployment folder
-  # e.g. "deployments/default/pihole/pihole-helm-values.yaml"
-  envsubst < "$@" | tee values.yaml \
-  | kubectl -n "${namespace}" create secret generic "${secret_name}" \
-    --from-file=values.yaml --dry-run=client -o json \
-  | kubeseal --format=yaml --cert="$PUB_CERT" \
-    > "${secret}.yaml"
-  # Clean up temp file
-  rm values.yaml
-}
-
 #
 # Helm Secrets
 #
 
-kseal "${REPO_ROOT}/deployments/default/radarr/radarr-helm-values.txt"
-kseal "${REPO_ROOT}/deployments/default/sonarr/sonarr-helm-values.txt"
-kseal "${REPO_ROOT}/deployments/default/nzbget/nzbget-helm-values.txt"
-kseal "${REPO_ROOT}/deployments/default/nzbhydra2/nzbhydra2-helm-values.txt"
-kseal "${REPO_ROOT}/deployments/default/home-assistant/home-assistant-db-helm-values.txt"
-kseal "${REPO_ROOT}/deployments/default/home-assistant/home-assistant-helm-values.txt"
-kseal "${REPO_ROOT}/deployments/kube-system/traefik-ingress/traefik-config-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/default/radarr/radarr-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/default/sonarr/sonarr-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/default/nzbget/nzbget-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/default/nzbhydra2/nzbhydra2-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/default/home-assistant/home-assistant-db-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/default/home-assistant/home-assistant-helm-values.txt"
+./kseal "${REPO_ROOT}/deployments/kube-system/traefik-ingress/traefik-config-helm-values.txt"
 
 #
 # Generic Secrets
