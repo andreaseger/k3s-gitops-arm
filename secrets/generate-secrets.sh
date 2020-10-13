fadd20f7e957ce25600dd2553928ddd2948845b5
Andreas Eger <dev@eger-andreas.de>
2020-10-13 21:43:21 +0200

    add sonarr, radarr & plex

--- secrets/generate-secrets.sh
+++ secrets/generate-secrets.sh
@@ -30,7 +30,7 @@ PUB_CERT="${REPO_ROOT}/secrets/pub-cert.pem"
 ./kseal "${REPO_ROOT}/deployments/default/nzbhydra2/nzbhydra2-helm-values.txt"
 ./kseal "${REPO_ROOT}/deployments/default/home-assistant/home-assistant-helm-values.txt"
 ./kseal "${REPO_ROOT}/deployments/default/pihole/pihole-helm-values.txt"
-
+./kseal "${REPO_ROOT}/deployments/default/plex/plex-helm-values.txt"
 
 ./kseal "${REPO_ROOT}/deployments/kube-system/traefik-ingress/traefik-config-helm-values.txt"
 
